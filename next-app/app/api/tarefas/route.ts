84c47255425bcb25ace7c372e666a7c5b4cf88c1
LuizFernando3 <luiz.etec2006@gmail.com>
2025-02-20 00:15:16 +0000

    aula19/02/25

---
+++ next-app/app/api/tarefas/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        mensagem: "Olá Next!!!"
+    });
+}
+
+export async function POST(){
+    return NextResponse.json({
+        mensagem:"Ola Next - POST!!!"
+    });
+}
